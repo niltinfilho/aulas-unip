957dc5b77f05af8d0110784a540a995bcfd62598
Nilton Filho <niltonfilho115@gmail.com>
2022-04-29 17:05:00 -0300

    exercicio

--- ex-aulas/exercícios/ex001/js/script.js
+++ ex-aulas/exercícios/ex001/js/script.js
@@ -1,7 +1,7 @@
 function somar() {
     var n1 = Number(document.getElementById('num1').value)
     var n2 = Number(document.getElementById('num2').value)
-    var res = n1 + n2
-    document.getElementById('res').innerHTML = res
-    document.getElementById('')
+    var resultado = n1 + n2
+    /* document.getElementById('res').value = resultado */
+    document.getElementById('res'). innerHTML = `A soma entre ${n1} e ${n2} é igual a ${resultado}`
 }
